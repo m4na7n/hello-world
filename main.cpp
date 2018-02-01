56fbb57ff2aec146c2479c2d23b30c7ad86a0335
Martin Andersson <martin.andersson@his.se>
2018-02-01 14:19:39 +0100

    Add hello4

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
     std::cout << "hello1" << std::endl;
     std::cout << "hello2" << std::endl;
     std::cout << "hello3" << std::endl;
+    std::cout << "hello4" << std::endl;
 
     return 0;
 }
